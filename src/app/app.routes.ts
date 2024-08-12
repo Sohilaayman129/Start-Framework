380070322b374bb5c13a6c74dd55508ab064ed5f
Sohilaayman129 <solyayman45@gmail.com>
2024-08-12 12:13:09 +0300

    sec

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+
+
+export const routes: Routes = [
+  {path:'', redirectTo:"home", pathMatch:'full' },
+  {path:'home', component:HomeComponent, title:'home'},
+    {path:'about', component:AboutComponent, title:'About'},
+    {path:'portfolio', component:PortfolioComponent, title:'Portfolio'},
+  {path:'contact', component:ContactComponent, title:'contact'},
+  {path:'**',component:NotfoundComponent, title:'404'}
+];
